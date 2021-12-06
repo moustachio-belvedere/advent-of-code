219783eacf5ac77e88ceecec7d569763c4d835ed
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2021-12-06 22:50:50 +0000

    day 3 part 1 done

--- 2021/racket/03-prog.rkt
+++ 2021/racket/03-prog.rkt
@@ -24,17 +24,37 @@
         1
         0)))
 
-;; Most Common Bit at index i
-(define (mcb bn i)
-  (ocb bn i >))
+;; * commont bit at all indices
+(define (*-common-bits bn op)
+  (define (biterator bn acc i len)
+    (if (>= i len)
+        acc
+        (biterator bn
+                   (append acc
+                           (list (ocb bn i op)))
+                   (+ i 1)
+                   len)))
 
-;; Least Common Bit at index i
-(define (lcb bn i)
-  (ocb bn i <))
+  (biterator bn '() 0 (length (car bn))))
 
-(define (bitsum lin acc)
-  (if (null? lin)
-      acc
+(define (most-common-bits bn)
+  (*-common-bits bn >))
+
+(define (least-common-bits bn)
+  (*-common-bits bn <))
+
+(define (binarylist->decimal bin)
+  (define (bitter bin i sum)
+    (let ((bit (car bin)))
+    (if (= i 0)
+        (+ sum bit)
+        (bitter (cdr bin)
+                (- i 1)
+                (if (zero? bit)
+                    sum
+                    (+ sum (expt 2 i)))))))
+
+  (bitter bin (- (length bin) 1) 0))
   
 (define (input-wrangle f)
   (let ((lines (file->lines f)))
@@ -42,10 +62,12 @@
                  (- (char->integer v) 48))
               (map string->list lines))))
 
+(define (power lines)
+  (* (binarylist->decimal (most-common-bits lines))
+     (binarylist->decimal (least-common-bits lines))))
+
 (module+ main
-  (define in (input-wrangle "03-testinput.txt"))
+  (define in (input-wrangle "03-realinput.txt"))
 
-  (mcb in 0)
-  (lcb in 0)
-
+  (power in)
 )
